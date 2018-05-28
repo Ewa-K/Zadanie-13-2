7564da36f1841a4ef657d42e81f1dec0a8e9ea66
Ewa-K <ewa-1988@yahoo.com>
2018-05-28 15:36:57 +0200

    Change app

--- modules/Time.js
+++ modules/Time.js
@@ -1,18 +1,17 @@
 function creatingTime(givenTime) {
 
+    var minutes = (givenTime / 60).toFixed(0);
+    var seconds = (givenTime % 60).toFixed(0);
+    var hours = (minutes / 60).toFixed(0);
+    var restOfHours = (minutes % 60).toFixed(0);
+
     if (givenTime !== null) {
-
-        var minutes = (givenTime / 60).toFixed(0);
-        var seconds = (givenTime % 60).toFixed(0);
-        var hours = (minutes / 60).toFixed(0);
-        var restOfHours = (minutes % 60).toFixed(0);
-
         if (givenTime < 60) {
-            console.log(givenTime, 'seconds');
+            return (givenTime + ' seconds ');
         } else if (givenTime >= 60 && givenTime < 3600) {
-            console.log(minutes, 'minutes', seconds, 'seconds');
+            return (minutes + ' minutes ' + seconds + ' seconds ');
         } else if (givenTime >= 3600) {
-            console.log(hours, 'hours', restOfHours, 'minutes', seconds, 'seconds');
+            return (hours + ' hours ' + restOfHours + ' minutes ' + seconds + ' seconds ');
         }
     }
 }
